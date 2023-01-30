2df15bf4a662b15ca45bcef0a76e4cd9fb2d1d10
bedok <bedokuroff.egor@gmail.com>
2023-01-30 17:27:16 +0200

    3nd commit - added cooments, second page

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -17,4 +17,18 @@ class Post(models.Model):
         verbose_name_plural = 'Записи'
 
 
+class Comments(models.Model):
+    '''база комментариев'''
+    email = models.EmailField()
+    name = models.CharField('Имя пользователя', max_length=40)
+    text_comments = models.TextField('Ваш комментарий', max_length=2000)
+    post = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE) # связывает таблицы с постами и комментариями
+
+    def __str__(self):
+        return f'{self.name}, {self.post}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
 
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -10,3 +10,11 @@ class PostView(View):
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'blog/blog.html', {'post_list': posts})
+
+
+class PostDitail(View):
+    '''отдельная страница записей для комментарий'''
+
+    def get(self, request, pk):
+        post = Post.objects.get(id=pk)
+        return render(request, 'blog/blog_ditail.html', {'post': post})
